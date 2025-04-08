b0a76a4aa1d0755f0f3dc3617939891c5cad679a
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-04-09 03:24:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,a,b):
+        set1 = set(a)
+        set2 = set(b)
+        set3 = set1.union(set2)
+        arr = list(set3)
+        arr.sort()
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Contributed by : Nagendra Jha
+# Modified by : Sagar Gupta
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        a = list(map(int, input().strip().split()))
+        b = list(map(int, input().strip().split()))
+        ob = Solution()
+        li = ob.findUnion(a, b)
+        for val in li:
+            print(val, end=' ')
+        print()
+        print("~")
+
+# } Driver Code Ends
